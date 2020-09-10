1bb90dfe099146f2590eb0b1d5c39d0378d84e26
MernaAdel <31679900+mernaadell@users.noreply.github.com>
2020-09-10 17:40:12 +0200

    Rest of sql commands

--- commands.sql
+++ commands.sql
@@ -1,5 +1,5 @@
 create database My_Company;
-create data if not exists base My_Company;
+create database if not exists  My_Company;
 use database My_Company; --d5alt feha
 drop database My_Company;
 drop database if exists My_Company;
@@ -98,7 +98,7 @@ CREATE TABLE Orders (
 );
 
 --add forign key after
-alter table orders add constrains ordering FOREIGN key (OrderID) REFERENCES class(id);--7tet constrain b3d l create
+alter table orders add  FOREIGN key (OrderID) REFERENCES class(id);--7tet constrain b3d l create
 
 --functios
 select left(text,2)from try;--awal 7arfen
@@ -259,4 +259,191 @@ select name,sum(points) from try group by name;
 select name,count(status) from try group by name;
 --having filter
 select name,count(status) howmuch from try group by name having howmuch >1;
+
+
+------------------------------------
+
+/*
+select * from table //attr l table kolha
+
+SELECT - extracts data from a database
+UPDATE - updates data in a database
+DELETE - deletes data from a database
+INSERT INTO - inserts new data into a database
+CREATE DATABASE - creates a new database
+ALTER DATABASE - modifies a database
+CREATE TABLE - creates a new table
+ALTER TABLE - modifies a table
+DROP TABLE - deletes a table
+CREATE INDEX - creates an index (search key)
+DROP INDEX - deletes an index
+
+SELECT column1, column2, ...
+FROM table_name;
+
+
+SELECT DISTINCT column1, column2, ... The SELECT DISTINCT statement is used to return only distinct (different) values.
+FROM table_name;
+
+SELECT COUNT(DISTINCT Country) FROM Customers; //3add l mo5tlfen
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition;  ex colum1>10 aw ay col mwgod fe table
+
+
+=	Equal	
+>	Greater than	
+<	Less than	
+>=	Greater than or equal	
+<=	Less than or equal	
+<>	Not equal. Note: In some versions of SQL this operator may be written as !=	
+BETWEEN	Between a certain range	
+LIKE	Search for a pattern	
+IN	To specify multiple possible values for a column
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition1 AND condition2 AND condition3 ...;
+
+and or not
+-----------------------------------------------
+-------------order by
+
+The ORDER BY keyword is used to sort the result-set in ascending or descending order.
+
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+
+
+ sorted by the "Country" and the "CustomerName" column. This means that it orders by Country, 
+ but if some rows have the same Country, it orders them by CustomerName:
+
+ SELECT * FROM Customers
+ORDER BY Country, CustomerName;
+
+---------insert into
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+------null values
+
+If a field in a table is optional, it is possible to insert a new record or update a record without adding a value to this field.
+ Then, the field will be saved with a NULL value.
+ mmkn asebha null wala la
+
+ ---------test null
+
+ SELECT column_names
+FROM table_name
+WHERE column_name IS NULL;
+
+----------update
+
+UPDATE Customers
+SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+WHERE CustomerID = 1;
+
+-----------delete
+
+DELETE FROM table_name WHERE condition;
+
+------- select top
+fe database l kbera b7dadlo 3wza shof mha ad eh
+
+SELECT TOP number|percent column_name(s)
+FROM table_name
+WHERE condition;
+
+SELECT TOP 50 PERCENT * FROM Customers;
+
+aw
+
+SELECT column_name(s)
+FROM table_name
+WHERE condition
+LIMIT number;
+
+----------min&nmax
+
+The MIN() function returns the smallest value of the selected column.
+
+The MAX() function returns the largest value of the selected column.
+
+
+SELECT MIN(column_name)
+FROM table_name
+WHERE condition;
+
+The COUNT() function returns the number of rows that matches a specified criterion.
+
+The AVG() function returns the average value of a numeric column.
+
+The SUM() function returns the total sum of a numeric column.
+
+------------like
+
+The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.
+
+There are two wildcards often used in conjunction with the LIKE operator:
+
+% - The percent sign represents zero, one, or multiple characters
+_ - The underscore represents a single character
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE columnN LIKE pattern;
+
+------------in
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
+
+-------------between
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name BETWEEN value1 AND value2;
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN "Carnarvon Tigers" AND "Chef Anton's Cajun Seasoning"
+ORDER BY ProductName;
+
+l text elii between dol
+
+-----------SQL Aliases
+tempo name 
+
+SELECT column_name AS alias_name
+FROM table_name;
+
+
+----------join
+
+A JOIN clause is used to combine rows from two or more tables,
+ based on a related column between them.
+
+ bged attr mo5tlfa mn tablesmo5tlfa bs based 3la attr mwgoda same fe l atnen
+
+select orederid,customername 
+
+ SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+
+
+SELECT column_name(s)
+FROM table1
+INNER JOIN table2
+ON table1.column_name = table2.column_name;
+
+
+
+
+
+
+*/
 
